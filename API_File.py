456047dee5e56595e9229c809df648f782feaf1c
Tarsem Singh <147481302+engg-TarsemSingh@users.noreply.github.com>
2025-09-01 12:53:53 +0530

    Update API_File.py
    changed method=["Post"] to "POST'

--- API_File.py
+++ API_File.py
@@ -25,7 +25,7 @@ maxi = df1["max"].iloc[0]
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/predict",methods=["Post"])
+@app.route("/predict",methods=["POST"])
 def predict():
     data = request.json
     lis = []
@@ -51,3 +51,4 @@ def predict():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
